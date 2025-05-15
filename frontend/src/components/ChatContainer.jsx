5cb50796afbf8928db89063ad333157694dd7d90
tidus <tidus23payos@gmail.com>
2025-05-15 17:10:44 +0800

    changed the avatar in chatcontainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -59,8 +59,8 @@ const ChatContainer = () => {
                 <img
                   src={
                     message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
+                      ? authUser.profilePic || defaultAvatar
+                      : selectedUser.profilePic || defaultAvatar
                   }
                   alt="profile pic"
                 />
